64f75a778ced16566e9cd9e9988ff3c56c21f27f
Ahnaf Shahriar <bdahnaf@gmail.com>
2018-06-21 01:45:07 +0600

    Add files via upload

---
+++ livesearch.php
@@ -0,0 +1,33 @@
+<?php
+
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+  $hint="";
+  $array = array();
+  $con=mysqli_connect("localhost","root","","demos");
+  $query=mysqli_query($con, "select * from cfg_demos where title LIKE '%{$q}%'");
+  while($row=mysqli_fetch_assoc($query))
+    {
+      //$array[] = $row['title'];
+	  $hint = $row['title'] . "<br />";
+    }
+  //echo json_encode($array);
+  //$hint = $array;
+  mysqli_close($con);
+}
+
+// Set output to "no suggestion" if no hint was found
+// or to the correct values
+if ($hint=="") {
+  $response="no suggestion";
+} else {
+  $response=$hint;
+}
+
+//output the response
+echo $response;
+?>
